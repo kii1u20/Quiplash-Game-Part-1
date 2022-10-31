e4876af4c47ea87c8f74b30f16803efc5f23705d
kii1u20 <kii1u20@soton.ac.uk>
2022-10-31 21:12:22 +0000

    Create prompt function working!

---
+++ CreatePrompt/__init__.py
@@ -0,0 +1,64 @@
+import json
+import logging
+import os
+
+import azure.functions as func
+import azure.cosmos as cosmos
+import azure.cosmos.exceptions as exceptions
+import config
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Processing Prompt request in progress....')
+
+    db_id = os.environ["db_id"]
+    db_key = os.environ["db_key"]
+    db_URI = os.environ["db_URI"]
+    users_container = os.environ["users_container"]
+    prompts_container = os.environ["prompts_container"]
+
+    # Create the needed proxy objects for CosmosDB account, database and tree container
+    client = cosmos.cosmos_client.CosmosClient(db_URI, db_key)
+
+    # Create a proxy object to the coursework1 Cosmos DB database
+    db_client = client.get_database_client(db_id)
+
+    # Create a proxy object to the users container
+    users_container = db_client.get_container_client(users_container)
+
+    prompts_container = db_client.get_container_client(prompts_container)
+
+
+    prompt = req.get_json()
+    username = prompt['username']
+    password = prompt['password']
+    text = prompt['text']
+    id = list(prompts_container.query_items(query="SELECT TOP 1 c.id FROM c ORDER BY c.id DESC", enable_cross_partition_query=True))
+    result = {}
+    if len(id) == 0:
+        result['id'] = "1"
+    else:
+        result['id'] = "{0}".format(int(id[0]['id']) + 1)
+    result['username'] = username
+    result['text'] = text
+
+    try:
+        prompt_exist = list(prompts_container.query_items(query= "SELECT * FROM c WHERE c.username = '{0}' AND c.text = '{1}'".format(username, text), 
+                            enable_cross_partition_query=True))
+        user_exists = list(users_container.query_items(query= "SELECT * FROM c WHERE c.username = '{0}'".format(username), enable_cross_partition_query = True))
+        if len(user_exists) == 0 :
+            return func.HttpResponse(body=json.dumps({"result": False, "msg": "bad username or password"}), status_code=409)
+        elif user_exists[0]['password'] != password:
+            return func.HttpResponse(body=json.dumps({"result": False, "msg": "bad username or password"}), status_code=409)
+        elif len(prompt_exist) != 0:
+                return func.HttpResponse(body=json.dumps({"result": False, "msg": "This user already has a prompt with the same text"}), status_code=409)
+        else:
+            if len(text) < 20 or len(text) > 100:
+                return func.HttpResponse(body=json.dumps({"result": False, "msg": "prompt length is <20 or > 100 characters"}), status_code=409)
+            else:
+                prompts_container.create_item(body=result)
+                return func.HttpResponse(body=json.dumps({"result" : True, "msg": "OK"}), status_code=200)
+    except exceptions.CosmosHttpResponseError as e:
+            print(e.message)
+            return func.HttpResponse("", status_code=404)
+    
---
+++ tests/test_add_prompt.py
@@ -0,0 +1,44 @@
+import unittest
+import json
+import requests 
+
+import azure.functions as func
+import azure.cosmos as cosmos
+import config
+#Important for the import name to match the case of the Function folder
+from RegisterPlayer import main
+
+class TestFunction(unittest.TestCase):
+
+    # note the config.settings to access configuration defined in config.py
+    client = cosmos.cosmos_client.CosmosClient(config.settings['db_URI'], config.settings['db_key'] )
+
+    # Create a proxy object to the treehuggers Cosmos DB database
+    db_client = client.get_database_client(config.settings['db_id'])
+
+    # Create a proxy object to the trees container
+    users_container = db_client.get_container_client(config.settings['users_container'])
+
+    def test_add_user(self):
+        payload = {"text" : "This prompt has to more than 0 characters", "username":  "alpha" , "password" : "test1234"}
+
+
+        resp = requests.get(
+                'https://coursework1-kii1u20.azurewebsites.net/api/CreatePrompt?code=42gZQ2Vvg-q9AON5QcF80P7N9XwK_Zy19TrMMKx8IXN7AzFuIVmh5w==', 
+                json = payload)
+
+
+        self.assertEqual({"result" : True, 'msg' : 'OK'}, resp.json())
+
+        # all_trees = list(self.users_container.read_all_items())
+        # self.assertEqual(len(all_trees),1)
+        # the_tree_in_the_db = all_trees[0]
+
+
+        # result = dict(itertools.islice(the_tree_in_the_db.items(), 4))
+
+        # current = result['id']
+        # result['id'] = int(current)
+
+        # self.assertEqual(payload['tree'], result)
+
